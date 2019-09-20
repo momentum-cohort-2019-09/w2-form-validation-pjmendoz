6a5f664bb70ad205407615abf9b820acfb764484
pjmendoz <pearl.j.mendoza@gmail.com>
2019-09-19 23:25:27 -0400

    adding javascript

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
-console.log('Add validation!');
+function q (selector){
+    return document.querySelector(selector)
+}
+
+function markValid (field){
+    field.parentNode.classList.remove("input-invalid")
+    field.parentNode.classList.add("input-valid")
+}
+
+function markInvalid (field){
+    field.parentNode.classList.remove("input-valid")
+    field.parentNode.classList.add("input-invalid")
+}    
+
+function main () {
+q('#parking-form').addEventListener('submit', function(event) {
+    event.preventDefault()
+    let nameField= q('.input-field')
+    let name= nameField.value.trim
+
+    if (!name) {
+        markInvalid(nameField)
+    } else {
+        markValid(nameField)
+    }   
+    
+})
+}
+
